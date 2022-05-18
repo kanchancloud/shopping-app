1f19c9718dda824b109cf0be97cd280562f382f8
kanchan <kanchansharma1408@gmail.com>
2022-05-18 14:33:32 +0530

    uninstall gh-pages remove deploy remove homepage from package.json on router put login page in bottom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import Header from "./component/Header";
 import HomePage from "./page/HomePage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import OrderPage from './page/OrderPage';
 import LoginProvider from "./contextApi";
-import {Login} from './auth/Login';
-import {Register} from './auth/Register';
+import { Login } from './auth/Login';
+import { Register } from './auth/Register';
 import React from "react";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 
 
 function App() {
     const loggedInStatus = useSelector((state) => state.userReducer.loggedIn)
-    let HideHeader = !loggedInStatus ? null : <Header />
-    return (
-        <div className="App">
-            <BrowserRouter>
-                {HideHeader}
-                    <Routes>
-                        <Route path="HomePage" exact element={<HomePage/>}/>
-                        <Route path="OrderPage" element={<OrderPage/>}/>
-                        <Route path="/" element={<Login/>}/>
-                        <Route path="/Register" element={<Register/>}/>
-                    </Routes>
-            </BrowserRouter>
-        </div>
-    );
+    let HideHeader = !loggedInStatus ? null : < Header / >
+        return ( <
+            div className = "App" >
+            <
+            BrowserRouter > { HideHeader } <
+            Routes >
+            <
+            Route path = "HomePage"
+            exact element = { < HomePage / > }
+            /> <
+            Route path = "OrderPage"
+            element = { < OrderPage / > }
+            /> <
+            Route path = "/Register"
+            element = { < Register / > }
+            /> <
+            Route exact path = "/"
+            element = { < Login / > }
+            /> <
+            /Routes> <
+            /BrowserRouter> <
+            /div>
+        );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import store, {persistor} from "./redux/store";
-import {Provider} from "react-redux";
-import {PersistGate} from "redux-persist/integration/react";
+import store, { persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <App/>
-            </PersistGate>
-        </Provider>
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    Provider store = { store } >
+    <
+    PersistGate persistor = { persistor } >
+    <
+    App / >
+    <
+    /PersistGate> <
+    /Provider>
 
-    </React.StrictMode>,
+    <
+    /React.StrictMode>,
     document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
